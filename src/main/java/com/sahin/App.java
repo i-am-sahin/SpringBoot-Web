67a5493f9b05405eb34cdfc3ec5d3972af0f21c1
Sahin Islam <sahinislam878@gmail.com>
2025-07-19 12:43:26 +0530

    Progress: Completed Running Tomcat Lecture from Udemy 2.0

--- src/main/java/com/sahin/App.java
+++ src/main/java/com/sahin/App.java
@@ -1,13 +1,18 @@
 package com.sahin;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
         System.out.println( "Hello World!" );
+        Tomcat tomcat  = new Tomcat();
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
--- src/main/java/com/sahin/HelloServlet.java
+++ src/main/java/com/sahin/HelloServlet.java
@@ -1,4 +1,17 @@
 package com.sahin;
 
-public class HelloServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res){
+        System.out.println("In Servlet");
+
+    }
+
 }
