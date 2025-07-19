8d0828b63ea49f7f795fe0a9090cd5904c29edb1
Sahin Islam <sahinislam878@gmail.com>
2025-07-19 13:16:34 +0530

    Progress: Completed Responding To The Client Lecture from Udemy

--- src/main/java/com/sahin/HelloServlet.java
+++ src/main/java/com/sahin/HelloServlet.java
@@ -4,14 +4,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException {
         System.out.println("In Servlet");
 
+
+//        res.getWriter().println("Hello World");
+//                    Or
+        res.setContentType("text/HTML"); //Telling the Browser we're sending html tags
+        PrintWriter out = res.getWriter();
+        out.println("<h2><b>Hello World!</b></h2>");
     }
 
 }
